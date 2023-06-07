2c4b862febcb806df8ac772bd3a210c1bfc1d112
lh <lemuel@haham.net>
2023-06-07 10:43:31 -0700

    analy

---
+++ analyzethis.php
@@ -0,0 +1,205 @@
+<html>
+<head>
+    <title>Upload Your Screenplay for AI Analysis | ScriptReader.ai</title>
+    <meta name="description" content="Upload your screenplay for in-depth AI analysis. Receive personalized feedback, grades, critiques, and suggestions to improve each scene. Start revolutionizing your screenplay with ScriptReader.ai today.">
+</head>
+<body>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/4f1cd0c3e5e45d73095866ff9f25503b5e7206e7/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<!-- Hotjar Tracking Code for script reader. ai -->
+<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-LR3WEHY0QH"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-LR3WEHY0QH'); </script>
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3483438,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1485423462196782');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1485423462196782&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- fixed-top"-->
+    <div class="container">
+      <a class="navbar-brand" href="/">There's no place like (scriptreader) home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Show me the money! (examples)</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="analyzethis.php">Analyze this! (your script)</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/blog">Dial B for Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">You talking to me? (contact)</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- Bootstrap JS -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <style>
+#overlay {
+  position: fixed;
+  display: none;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+}
+
+#overlay.show {
+  display: block;
+}
+
+#overlay:after {
+  content: 'Uploading...';
+  display: block;
+  font-size: 3em;
+  font-weight: bold;
+  text-align: center;
+  color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+    body {
+      background-color: black;
+      color: white;
+      font-family: Arial, sans-serif;
+    }
+    
+    h1 {
+      text-align: center;
+      font-size: 3em;
+      margin-top: 1em;
+    }
+    
+    form {
+//      width: 50%;
+      margin: 2em auto;
+      border: 2px solid white;
+      padding: 2em;
+      border-radius: 10px;
+    }
+    
+    label {
+      display: block;
+      margin-bottom: 1em;
+      font-size: 1.2em;
+    }
+    
+    input[type="text"],
+    input[type="email"],
+    input[type="file"],
+    input[type="submit"] {
+      display: block;
+      margin-bottom: 1em;
+   //  font-size: 1.2em;
+      padding: 0.5em;
+      border-radius: 5px;
+      border: none;
+      background-color: #222;
+      color: white;
+    }
+    
+    input[type="submit"] {
+      background-color: #f00;
+      color: white;
+      cursor: pointer;
+    }
+    
+    input[type="submit"]:hover {
+      background-color: #c00;
+    }
+    
+    #promo-code {
+      display: block;
+      margin-bottom: 1em;
+      font-size: 1.2em;
+      padding: 0.5em;
+      border-radius: 5px;
+      border: none;
+      background-color: #eee;
+      color: #222;
+      font-weight: bold;
+    }
+  </style>
+  <div class="container">
+
+ <h1> Analyze this!</h1>
+<br/><br/>
+
+ <center> <h3>Upload your screenplay in PDF.</h3>
+<br/>
+<p>Examples of what to expect can be found <a href="/gallery.php">here</a>.</center>
+
+  <form id="file-upload-form" action="upload.php" method="post" enctype="multipart/form-data">
+  <div id="response"></div>
+    <label for="title">Movie Title:</label>
+    <input type="text" name="title" id="title" required>
+    <label for="email">Your Email:</label>
+    <input type="email" name="email" id="email" required>
+    <label for="file">Movie File in PDF format:</label>
+    <input type="file" name="file" id="file" required>
+	    <!--label for="promo-code">Voucher Code:</label-->
+    <!--input type="text" name="code" id="code"-->
+<label>
+  <input type="checkbox" name="terms" required>
+  I agree to the Terms and Conditions
+</label>
+    <input type="submit" value="Upload Movie">
+  <div id="overlay"></div>
+  </form>
+
+</div>
+</body>
+</html>
